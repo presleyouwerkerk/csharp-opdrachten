9b9d4291643416c6c3b9aebc6c9dfda07539e110
Presley Ouwerkerk <presley2225@gmail.com>
2024-07-25 05:44:24 +0200

    change

---
+++ StudentStaffConsoleApplication/StudentStaffConsoleApplication/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentStaffConsoleApplication
+{
+    internal class Person(string name, string address)
+    {
+        public string Name { get; private set; } = name;
+        public string Address { get; private set; } = address;
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetAddress()
+        {
+            return Address;
+        }
+
+        public void SetAddress(string address)
+        {
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"Person : name={Name}, address={Address}";
+        }
+    }
+}
